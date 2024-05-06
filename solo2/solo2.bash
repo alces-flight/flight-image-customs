d9551a1a1ff4e413f08e0ec81783277a93d7e74c
Stu Franks <stu@stuts.uk>
2024-05-06 21:15:28 +0100

    Use alces repo to get brand prompt

--- solo2/solo2.bash
+++ solo2/solo2.bash
@@ -11,6 +11,12 @@ name=OpenFlight - Base
 baseurl=https://repo.openflighthpc.org/openflight/centos/$releasever/$basearch/
 enabled=1
 gpgcheck=0
+
+[alcesflight]
+name=AlcesFlight - Base
+baseurl=https://alces-flight.s3-eu-west-1.amazonaws.com/repos/alces-flight/centos/$releasever/$basearch/
+enabled=1
+gpgcheck=0
 EOF
 
 CENTOS_VER=$(rpm --eval '%{centos_ver}')
